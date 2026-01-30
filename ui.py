5727b66417517c9a6ff4052e726a8d574ea2e51a
Nemanja <nemanjavasa@gmail.com>
2026-01-30 08:49:45 +0100

    chore: Use text area instead of text input

--- ui.py
+++ ui.py
@@ -10,9 +10,9 @@ if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
 
 if "report_data" not in st.session_state:
-    st.session_state.report_data = None
+    st.session_state.tool_report_resp = None
 
-ai_tool = st.text_input("Write the name of the AI tool to assess", placeholder="e.g. Notion AI")
+ai_tool = st.text_area("Write the name of the AI tool to assess", placeholder="e.g. Notion AI")
 
 if st.button("Submit"):
     if not ai_tool:
@@ -23,10 +23,10 @@ if st.button("Submit"):
             response = requests.post("http://localhost:8000/run", json=payload)
 
             if response.ok:
-                st.session_state.report_data = response.json().get("summary")
+                st.session_state.tool_report_resp = response.json().get("summary")
             else:
                 st.error("Failed to assess AI tool.")
 
-if st.session_state.report_data:
+if st.session_state.tool_report_resp:
     st.divider()
-    st.markdown(st.session_state.report_data)
+    st.markdown(st.session_state.tool_report_resp)
